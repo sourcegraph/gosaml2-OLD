fdabe5c9ffebe7cfc063b4cb5fac8420fd5ed8c3
Phoebe Simon <phoebesimon@gmail.com>
2016-03-29 15:08:22 -0700

    Verify the conditions of an Assertion, report assertion values and warnings
    saml.go: moved parsing the assertion info into here, verify the conditions, pass back values and assertion warnings
    saml_test.go: added test cases for the various ways to produce warnings/errors
    xml_constants.go: xml tag/attribute constants
    test_constants.go: these were getting cumbersome, so moved them to their own file

--- saml.go
+++ saml.go
@@ -3,7 +3,9 @@ package saml2
 import (
 	"bytes"
 	"encoding/base64"
+	"errors"
 	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/beevik/etree"
@@ -18,6 +20,7 @@ type SAMLServiceProvider struct {
 	IdentityProviderIssuer      string
 	AssertionConsumerServiceURL string
 	SignAuthnRequests           bool
+	AudienceURI                 string
 	IDPCertificateStore         dsig.X509CertificateStore
 	SPKeyStore                  dsig.X509KeyStore
 }
@@ -125,11 +128,90 @@ func (sp *SAMLServiceProvider) ValidateEncodedResponse(encodedResponse string) e
 	return nil
 }
 
-func (sp *SAMLServiceProvider) RetrieveAssertionInfo(encodedResponse string) (map[string]string, error) {
+type proxyRestriction struct {
+	Count    int
+	Audience []string
+}
+
+type warningInfo struct {
+	OneTimeUse       bool
+	ProxyRestriction *proxyRestriction
+	NotInAudience    bool
+}
+
+type AssertionInfo struct {
+	Values      map[string]string
+	WarningInfo *warningInfo
+}
+
+func childPath(space, tag string) string {
+	if space == "" {
+		return "./" + tag
+	} else {
+		return "./" + space + ":" + tag
+	}
+}
+
+func (sp *SAMLServiceProvider) VerifyAssertionConditions(assertionElement, conditionsStatement *etree.Element) (*warningInfo, error) {
+	warningInfo := &warningInfo{}
+
+	audienceRestrictionStatement := conditionsStatement.FindElement(childPath(assertionElement.Space, AudienceRestrictionTag))
+	if audienceRestrictionStatement != nil {
+		audienceStatements := audienceRestrictionStatement.FindElements(childPath(assertionElement.Space, AudienceTag))
+		if len(audienceStatements) == 0 {
+			return nil, errors.New("Missing AudienceStatement")
+		}
+
+		matched := false
+		for _, audienceStatement := range audienceStatements {
+			if audienceStatement.Text() == sp.AudienceURI {
+				matched = true
+			}
+		}
+
+		if !matched {
+			warningInfo.NotInAudience = true
+		}
+	}
+
+	oneTimeUseStatement := conditionsStatement.FindElement(childPath(assertionElement.Space, OneTimeUseTag))
+	if oneTimeUseStatement != nil {
+		warningInfo.OneTimeUse = true
+	}
+
+	proxyRestrictionStatement := conditionsStatement.FindElement(childPath(assertionElement.Space, ProxyRestrictionTag))
+	if proxyRestrictionStatement != nil {
+		proxyRestrictionInfo := &proxyRestriction{}
+		countAttr := proxyRestrictionStatement.SelectAttr(CountAttr)
+		if countAttr != nil {
+			count, err := strconv.Atoi(countAttr.Value)
+			if err != nil {
+				return nil, errors.New("Could not parse Count attribute")
+			}
+
+			proxyRestrictionInfo.Count = count
+		}
+
+		proxyAudienceStatements := proxyRestrictionStatement.FindElements(childPath(assertionElement.Space, AudienceTag))
+		pas := make([]string, len(proxyAudienceStatements))
+		for i, proxyAudienceStatement := range proxyAudienceStatements {
+			pas[i] = proxyAudienceStatement.Text()
+		}
+
+		proxyRestrictionInfo.Audience = pas
+		warningInfo.ProxyRestriction = proxyRestrictionInfo
+	}
+
+	return warningInfo, nil
+}
+
+func (sp *SAMLServiceProvider) RetrieveAssertionInfo(encodedResponse string) (*AssertionInfo, error) {
 	raw, err := base64.StdEncoding.DecodeString(encodedResponse)
 	if err != nil {
 		return nil, err
 	}
+
+	assertionInfo := &AssertionInfo{}
 
 	doc := etree.NewDocument()
 	err = doc.ReadFromBytes(raw)
@@ -137,5 +219,64 @@ func (sp *SAMLServiceProvider) RetrieveAssertionInfo(encodedResponse string) (ma
 		return nil, err
 	}
 
-	return sp.validationContext().RetrieveAssertionInfo(doc.Root())
+	el := doc.Root()
+
+	assertionElement := el.FindElement("//" + AssertionTag)
+	if assertionElement == nil {
+		return nil, errors.New("Missing Assertion")
+	}
+
+	//Verify all conditions for the assertion
+	conditionsStatement := assertionElement.FindElement(childPath(assertionElement.Space, ConditionsTag))
+	if conditionsStatement == nil {
+		return nil, errors.New("Missing ConditionsStatement")
+	}
+	now := time.Now()
+
+	notBeforeAttr := conditionsStatement.SelectAttr(NotBeforeAttr)
+	if notBeforeAttr != nil {
+		before, err := time.Parse(time.RFC3339, notBeforeAttr.Value)
+		if err != nil {
+			return nil, errors.New("Could not parse 'NotBefore' attribute")
+		}
+
+		if now.Before(before) {
+			return nil, errors.New("Assertion statement is not valid yet")
+		}
+	}
+	notOnOrAfterAttr := conditionsStatement.SelectAttr(NotOnOrAfterAttr)
+	if notBeforeAttr != nil {
+		after, err := time.Parse(time.RFC3339, notOnOrAfterAttr.Value)
+		if err != nil {
+			return nil, errors.New("Could not parse 'NotOnOrAfter' attribute")
+		}
+		if now.After(after) {
+			return nil, errors.New("Assertion statement is no longer valid")
+		}
+	}
+
+	warningInfo, err := sp.VerifyAssertionConditions(assertionElement, conditionsStatement)
+	if err != nil {
+		return nil, err
+	}
+
+	//Get the actual assertion attributes
+	attributeStatement := assertionElement.FindElement(childPath(assertionElement.Space, AttributeStatementTag))
+	if attributeStatement == nil {
+		return nil, errors.New("Missing AttributeStatement")
+	}
+
+	info := make(map[string]string)
+	for _, child := range attributeStatement.ChildElements() {
+		nameAttr := child.SelectAttr(NameAttr)
+		attributeValue := child.FindElement(childPath(child.Space, AttributeValueTag))
+		if attributeValue == nil {
+			return nil, errors.New("Missing AttributeValue")
+		}
+		info[nameAttr.Value] = attributeValue.Text()
+	}
+
+	assertionInfo.Values = info
+	assertionInfo.WarningInfo = warningInfo
+	return assertionInfo, nil
 }
---
+++ xml_constants.go
@@ -0,0 +1,19 @@
+package saml2
+
+const (
+	AssertionTag           = "Assertion"
+	AttributeStatementTag  = "AttributeStatement"
+	AttributeValueTag      = "AttributeValue"
+	ConditionsTag          = "Conditions"
+	AudienceRestrictionTag = "AudienceRestriction"
+	AudienceTag            = "Audience"
+	OneTimeUseTag          = "OneTimeUse"
+	ProxyRestrictionTag    = "ProxyRestriction"
+)
+
+const (
+	NameAttr         = "Name"
+	NotBeforeAttr    = "NotBefore"
+	NotOnOrAfterAttr = "NotOnOrAfter"
+	CountAttr        = "Count"
+)

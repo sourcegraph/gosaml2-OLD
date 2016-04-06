ef830d3f5e9f21d0f0500938fa71bf563d6caad7
Russell Haering <russellhaering@gmail.com>
2016-04-06 15:10:17 -0700

    Update the example

--- s2example/demo.go
+++ s2example/demo.go
@@ -1,21 +1,62 @@
 package main
 
 import (
-	"io"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"net/http"
 
 	"github.com/russellhaering/gosaml2"
 	"github.com/russellhaering/goxmldsig"
 )
 
+var rawIdpCertificate = `
+-----BEGIN CERTIFICATE-----
+MIIDpDCCAoygAwIBAgIGAVLIBhAwMA0GCSqGSIb3DQEBBQUAMIGSMQswCQYDVQQGEwJVUzETMBEG
+A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
+MBIGA1UECwwLU1NPUHJvdmlkZXIxEzARBgNVBAMMCmRldi0xMTY4MDcxHDAaBgkqhkiG9w0BCQEW
+DWluZm9Ab2t0YS5jb20wHhcNMTYwMjA5MjE1MjA2WhcNMjYwMjA5MjE1MzA2WjCBkjELMAkGA1UE
+BhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xDTALBgNV
+BAoMBE9rdGExFDASBgNVBAsMC1NTT1Byb3ZpZGVyMRMwEQYDVQQDDApkZXYtMTE2ODA3MRwwGgYJ
+KoZIhvcNAQkBFg1pbmZvQG9rdGEuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+mtjBOZ8MmhUyi8cGk4dUY6Fj1MFDt/q3FFiaQpLzu3/q5lRVUNUBbAtqQWwY10dzfZguHOuvA5p5
+QyiVDvUhe+XkVwN2R2WfArQJRTPnIcOaHrxqQf3o5cCIG21ZtysFHJSo8clPSOe+0VsoRgcJ1aF4
+2rODwgqRRZdO9Wh3502XlJ799DJQ23IC7XasKEsGKzJqhlRrfd/FyIuZT0sFHDKRz5snSJhm9gpN
+uQlCmk7ONZ1sXqtt+nBIfWIqeoYQubPW7pT5GTc7wouWq4TCjHJiK9k2HiyNxW0E3JX08swEZi2+
+LVDjgLzNc4lwjSYIj3AOtPZs8s606oBdIBni4wIDAQABMA0GCSqGSIb3DQEBBQUAA4IBAQBMxSkJ
+TxkXxsoKNW0awJNpWRbU81QpheMFfENIzLam4Itc/5kSZAaSy/9e2QKfo4jBo/MMbCq2vM9TyeJQ
+DJpRaioUTd2lGh4TLUxAxCxtUk/pascL+3Nn936LFmUCLxaxnbeGzPOXAhscCtU1H0nFsXRnKx5a
+cPXYSKFZZZktieSkww2Oi8dg2DYaQhGQMSFMVqgVfwEu4bvCRBvdSiNXdWGCZQmFVzBZZ/9rOLzP
+pvTFTPnpkavJm81FLlUhiE/oFgKlCDLWDknSpXAI0uZGERcwPca6xvIMh86LjQKjbVci9FYDStXC
+qRnqQ+TccSu/B6uONFsDEngGcXSKfB+a
+-----END CERTIFICATE-----
+`
+
 func main() {
+	// Load the identity provider's signing certificate. In this case we are using
+	// an Okta developer account.
+	block, _ := pem.Decode([]byte(rawIdpCertificate))
+	idpCert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	certStore := dsig.MemoryX509CertificateStore{
+		Roots: []*x509.Certificate{idpCert},
+	}
+
+	// We sign the AuthnRequest with a random key because Okta doesn't seem
+	// to verify these.
+	randomKeyStore := dsig.RandomKeyStoreForTest()
+
 	sp := &saml2.SAMLServiceProvider{
-		IdentityProviderSSOURL:      "https://dev-116807.oktapreview.com/app/scaleftdev116807_scaleft_1/exk5zt0r12Edi4rD20h7/sso/saml",
-		IdentityProviderIssuer:      "http://www.okta.com/exk5zt0r12Edi4rD20h7",
+		IdentityProviderSSOURL:      "https://dev-116807.oktapreview.com/app/scaleftdev116807_test_1/exk659aytfMeNI49v0h7/sso/saml",
+		IdentityProviderIssuer:      "http://www.okta.com/exk659aytfMeNI49v0h7",
 		AssertionConsumerServiceURL: "http://localhost:8080/v1/_saml_callback",
 		SignAuthnRequests:           true,
-		IDPCertificateStore:         nil,
-		SPKeyStore:                  dsig.RandomKeyStoreForTest(),
+		AudienceURI:                 "123",
+		IDPCertificateStore:         &certStore,
+		SPKeyStore:                  randomKeyStore,
 	}
 
 	http.HandleFunc("/v1/_saml_callback", func(rw http.ResponseWriter, req *http.Request) {
@@ -24,12 +65,12 @@ func main() {
 			panic(err)
 		}
 
-		err = sp.ValidateEncodedResponse(req.FormValue("SAMLResponse"))
+		assertionInfo, err := sp.RetrieveAssertionInfo(req.FormValue("SAMLResponse"))
 		if err != nil {
 			panic(err)
 		}
 
-		io.WriteString(rw, "nope")
+		fmt.Fprintf(rw, "%v\n", assertionInfo)
 	})
 
 	authUrl, err := sp.BuildAuthURL("")
